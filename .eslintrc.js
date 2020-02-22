c480281c63e31ae1862d29a6872e4b7d50a09389
Thibault <melkir13@gmail.com>
2020-02-22 20:53:41 +0100

    (core) update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['@typescript-eslint', 'jest', 'import'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
   ],
   rules: {
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off'
-  }
-};
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+    'no-console': 'error',
+    'object-shorthand': ['warn', 'always', { avoidExplicitReturnArrows: true }],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+  },
+}
